f97025502ae760fe45f36e24ce8fdb768d0bc7e5
Arrovalos <marcelo.avalosmtz@gmail.com>
2025-08-29 06:20:18 -0600

    Añadí función de enlistar los nombres

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 <h1>Primer Challenge: AMIGO SECRETO</h1>
+```Qué debo hacer:```
+-hacer un array con datos string recibidos en el prompt
+-elegir una posición random del array
+-mostrar resultado en pantalla
+-reiniciar al recibir nuevos datos
--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let nombreAmigos = [];
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function mostrarMensajeTemporal(selector, mensaje, duracion = 1000) {
+    let elementoHTML = document.querySelector(selector);
+    const mensajeOriginal = elementoHTML.innerHTML;
+    
+    asignarTextoElemento(selector, mensaje);
+    
+    setTimeout(() => {
+        asignarTextoElemento(selector, mensajeOriginal);
+    }, duracion);
+}
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value.trim();
+    if (amigo === " ") {
+        asignarTextoElemento('h2','Por favor escribe un nombre válido');
+        return 
+    }
+
+    if (!isNaN(amigo)) {
+        asignarTextoElemento('h2','Por favor escribe un nombre válido');
+        return 
+    }
+    nombreAmigos.push(amigo);
+    console.log(nombreAmigos);
+    console.log(nombreAmigos.length)
+    limpiarCaja();
+    mostrarMensajeTemporal('h2', '¡Amigo agregado exitosamente!', 1000);
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
